375ff7c689514da7e7fbc1b6f232644851af9a6f
Rafael Pereira <rafaelps@dcomp.ufs.br>
2023-05-08 17:31:19 -0300

    Able delete user; Improvements the user controllers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: %i[show destroy]
+
   # GET /users/1
   def show
-    user = User.find_by_id(params[:id])
-    render json: user and return if user.present?
-
+    render json: @user and return if @user.present?
     render json: { message: 'Not found' }, status: :not_found
   end
 
@@ -15,9 +15,19 @@ class Api::V1::UsersController < ApplicationController
     render json: user.errors, status: :unprocessable_entity
   end
 
+  # DELETE /users/1
+  def destroy
+    @user.destroy
+    head 204
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:email, :password)
+  end
+
+  def set_user
+    @user = User.find_by_id(params[:id])
   end
 end
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Users", type: :request do
-  describe "GET /show" do
+RSpec.describe 'Api::V1::Users', type: :request do
+  describe 'GET /show' do
     before do
-      user = FactoryBot.create(:user)
-      get "/api/v1/users/#{user.id}"
+      @user = FactoryBot.create(:user)
+      get "/api/v1/users/#{@user.id}"
     end
 
     it 'return user' do
@@ -12,7 +12,22 @@ RSpec.describe "Api::V1::Users", type: :request do
     end
 
     it 'not found user' do
-      get "/api/v1/users/12345678"
+      get '/api/v1/users/12345678'
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
+  describe 'DELETE /destroy' do
+    before do
+      @user = FactoryBot.create(:user)
+      get "/api/v1/users/#{@user.id}"
+    end
+
+    it 'delete user' do
+      delete "/api/v1/users/#{@user.id}"
+      expect(response).to have_http_status(:no_content)
+
+      get "/api/v1/users/#{@user.id}"
       expect(response).to have_http_status(:not_found)
     end
   end
